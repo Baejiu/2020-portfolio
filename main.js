afbeecef79d26d3b5326a0e3146045992eb4575c
Baejiu <57855548+Baejiu@users.noreply.github.com>
2020-08-25 17:42:39 +0900

    JS: Project filter Btn

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-'use strict'
+"use strict";
 
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector('#navbar');
+const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-  if(window.scrollY > navbarHeight) {
-    navbar.classList.add('navbar--dark');
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
   } else {
-    navbar.classList.remove('navbar--dark');
+    navbar.classList.remove("navbar--dark");
   }
 });
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event) => {
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
   const target = event.target;
   const link = target.dataset.link;
-  if (link ==null) {
-return;
+  if (link == null) {
+    return;
   } else {
     // const scrollTo = document.querySelector(link);
     // scrollTo.scrollIntoView({behavior: 'smooth'});
-  scrollIntoView(link);
+    scrollIntoView(link);
   }
 });
 
@@ -35,11 +35,10 @@ return;
 // });
 
 //Handle click on "contact me" button on home - ellie
-const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click', () => {
-  scrollIntoView('#contact');
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
 });
-
 
 //Transparent home -jiu
 // const home = document.querySelector('#home');
@@ -48,13 +47,13 @@ homeContactBtn.addEventListener('click', () => {
 // document.addEventListener('scroll', () => {
 //   const y = window.scrollY;
 //   console.log(y);
-//   if(y / homeHeight) {} 
+//   if(y / homeHeight) {}
 // });
 
 //Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector('.home__container');
+const home = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
+document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
@@ -68,22 +67,42 @@ document.addEventListener('scroll', () => {
 // }
 
 // Show "arrow up" button when scrolling down - ellie
-const arrowUp = document.querySelector('.arrow-up')
-document.addEventListener('scroll', () => {
-  if(window.scrollY > homeHeight/2) {
-    arrowUp.classList.add('visible');
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
   } else {
-    arrowUp.classList.remove('visible');
+    arrowUp.classList.remove("visible");
   }
 });
 
 //Handle click on the "arrow up" button
-arrowUp.addEventListener('click', () => {
-  scrollIntoView('#home');
+arrowUp.addEventListener("click", () => {
+  scrollIntoView("#home");
 });
 
+//Projects
+const workBtncontainer = document.querySelector(".work__categories");
+const ProjectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+
+workBtncontainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+  projects.forEach((project) => {
+    console.log(project.dataset.type);
+    if (filter === "*" || filter === project.dataset.type) {
+      project.classList.remove("invisible");
+    } else {
+      project.classList.add("invisible");
+    }
+  });
+});
+
+///
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth'});
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 }
-
