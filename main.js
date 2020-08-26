0d7047e047971e74560cb8f18d9894cb0e412f31
Baejiu <57855548+Baejiu@users.noreply.github.com>
2020-08-26 13:10:45 +0900

    JS: single quote

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector("#navbar");
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
+    navbar.classList.add('navbar--dark');
   } else {
-    navbar.classList.remove("navbar--dark");
+    navbar.classList.remove('navbar--dark');
   }
 });
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener("click", (event) => {
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
@@ -35,9 +35,9 @@ navbarMenu.addEventListener("click", (event) => {
 // });
 
 //Handle click on "contact me" button on home - ellie
-const homeContactBtn = document.querySelector(".home__contact");
-homeContactBtn.addEventListener("click", () => {
-  scrollIntoView("#contact");
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact');
 });
 
 //Transparent home -jiu
@@ -51,9 +51,9 @@ homeContactBtn.addEventListener("click", () => {
 // });
 
 //Make home slowly fade to transparent as the window scrolls down
-const home = document.querySelector(".home__container");
+const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
@@ -67,42 +67,45 @@ document.addEventListener("scroll", () => {
 // }
 
 // Show "arrow up" button when scrolling down - ellie
-const arrowUp = document.querySelector(".arrow-up");
-document.addEventListener("scroll", () => {
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
   if (window.scrollY > homeHeight / 2) {
-    arrowUp.classList.add("visible");
+    arrowUp.classList.add('visible');
   } else {
-    arrowUp.classList.remove("visible");
+    arrowUp.classList.remove('visible');
   }
 });
 
 //Handle click on the "arrow up" button
-arrowUp.addEventListener("click", () => {
-  scrollIntoView("#home");
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
 });
 
 //Projects
-const workBtncontainer = document.querySelector(".work__categories");
-const ProjectContainer = document.querySelector(".work__projects");
-const projects = document.querySelectorAll(".project");
+const workBtncontainer = document.querySelector('.work__categories');
+const ProjectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
 
-workBtncontainer.addEventListener("click", (e) => {
+workBtncontainer.addEventListener('click', (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  console.log(e.target.dataset);
   if (filter == null) {
     return;
   }
   projects.forEach((project) => {
     console.log(project.dataset.type);
-    if (filter === "*" || filter === project.dataset.type) {
-      project.classList.remove("invisible");
+    if (filter === '*' || filter === project.dataset.type) {
+      project.classList.remove('invisible');
     } else {
-      project.classList.add("invisible");
+      project.classList.add('invisible');
     }
   });
 });
 
-///
+//
+//
+//
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
