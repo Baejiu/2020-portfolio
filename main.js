f10dab3b6ab5d31d6503726e606e80ac48e506a6
Baejiu <57855548+Baejiu@users.noreply.github.com>
2020-08-30 15:04:28 +0900

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ document.addEventListener('scroll', () => {
   }
 });
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -21,8 +22,15 @@ navbarMenu.addEventListener('click', (event) => {
   } else {
     // const scrollTo = document.querySelector(link);
     // scrollTo.scrollIntoView({behavior: 'smooth'});
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
   }
+});
+
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 //Handle contact me button - jiu
